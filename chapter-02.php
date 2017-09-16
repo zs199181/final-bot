a8f733bb32bd5d408a1e0ccefd6d0c63bf4bb624
sira saklertwilai <jean50fuji@gmail.com>
2017-09-16 12:42:58 +0700

    change key in chapter-02.php

--- chapter-02.php
+++ chapter-02.php
@@ -2,12 +2,13 @@
 
 include ('line-bot-api/php/line-bot.php');
 
-$channelSecret = '551ec4........3cff0';
-$access_token  = '2og9ogezC..... W5ZUEQQdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '	
+4c6b31ffceb1b59b3c93ac27345062f8';
+$access_token  = '1fv4VHaVDl8Me9nQlLF3UUbLjPOdiR/G405XuGQ5qtqxlX4e3ka8r6v6N2Q6Qm693qnMwLv/2dQdNRiJ6vCGMc2ea6is/Lofj2vvB/OwYgN81xpbRwr0FIVE6fsuGxriuO3nFP1HefTB5Xo65XiiUAdB04t89/1O/w1cDnyilFU=';
 
 $bot = new BOT_API($channelSecret, $access_token);
 	
-$bot->sendMessageNew('U39f72cc.....d460d6ddf', 'Hello World !!');
+$bot->sendMessageNew('U9e0920885592118c68d68f65761d9c3f', 'Hello World !!');
 
 if ($bot->isSuccess()) {
     echo 'Succeeded!';
